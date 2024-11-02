09fc6fabd724c1a0b5145350bcd388928daac148
this override <n4yor93@hotmail.fr>
2024-11-02 09:02:35 +0100

    define structure

--- src/adapter/controller/EmailController.ts
+++ src/adapter/controller/EmailController.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from 'express';
 import EmailService from '../services/EmailService';
-import { POST } from '../../framework/express/hotspring/hotSpring'; 
+import { POST } from '../../framework/express/hotspring/hotSpring';
 import { inject, injectable } from 'inversify';
 
 @injectable()
 export default class EmailController {
   private emailService: EmailService;
 
-  constructor(@inject(EmailService) emailService : EmailService) {
-    this.emailService = new EmailService();
+  constructor(@inject(EmailService) emailService: EmailService) {
+    this.emailService = emailService;
   }
 
-  @POST('/send')
+  @POST('/sendmessage')
   public async sendEmail(request: Request, response: Response): Promise<void> {
     try {
-      const serviceResponse = await this.emailService.sendEmail(request.body);
-      response.status(serviceResponse.status).json({ message: serviceResponse.data });
+      const { status, data } = await this.emailService.sendEmail(request.body);
+      response.status(status).json(data);
     } catch (error: any) {
       console.error(error);
       response.status(500).json({ message: 'Erreur interne du serveur' });
--- src/framework/express/ExpressApp.ts
+++ src/framework/express/ExpressApp.ts
@@ -1,11 +1,12 @@
 import express, { Application } from 'express';
 import { Container } from 'inversify';
 import HotSpring from './hotspring/core/HotSpring';
-import mongoose, { ConnectOptions } from 'mongoose';
+// import mongoose, { ConnectOptions } from 'mongoose';
 import EmailService from '../../adapter/services/EmailService';
 import EmailController from '../../adapter/controller/EmailController';
 import { configureMiddleware } from './config/middleware';
 import { configureErrorHandling } from './config/errorHandling';
+import TrackingController from '../../adapter/controller/TrackingController';
 
 
 export default class ExpressApp {
@@ -22,22 +23,24 @@ export default class ExpressApp {
   private _initializeIoCContainer(): void {
     this.IoCContainer.bind<EmailService>(EmailService).toSelf();
     this.IoCContainer.bind<EmailController>(EmailController).toSelf();
+    this.IoCContainer.bind<TrackingController>(TrackingController).toSelf();
   }
 
   private _configureApp(): void {
     configureMiddleware(this.app)
     HotSpring.bind(this.app, this.IoCContainer, EmailController)
+    HotSpring.bind(this.app, this.IoCContainer, TrackingController)
     configureErrorHandling(this.app)
   }
 
   public async run(port: number): Promise<void> {
     try {
       this.app.listen(port, async () => {
-        await mongoose.connect(process.env.MONGODB_URI as string, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        } as ConnectOptions );
-        console.info('Connected to MongoDB');
+        // await mongoose.connect(process.env.MONGODB_URI as string, {
+        //   useNewUrlParser: true,
+        //   useUnifiedTopology: true,
+        // } as ConnectOptions );
+        // console.info('Connected to MongoDB');
         console.info('\x1b[1m\x1b[36m%s\x1b[0m', `User Service on http://localhost:${port}`);
       });
     } catch (error) {
